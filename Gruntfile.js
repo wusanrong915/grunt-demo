e5ce819f1be1bd22470a60b849c67628a2a779f2
wusanrong <2085067543@qq.com>
2019-03-19 14:47:34 +0800

    tijiao

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt){
+  grunt.initConfig{
+    less:{
+      conpile:{
+        files:{'dist/compiled.css':'css/*.less'}
+      }                 
+    }
+  });
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.registerTask('default',['less:compile']);
+};
